66b9bc9a2b0d15d06eea395a1ca173b560cb0749
Your Name <you@example.com>
2024-12-24 13:05:27 +0100

    added shinyness

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@ use pokemon::Pokemon;
 use rng_factory::RngFactory;
 
 fn main() {
+    // hardcoded input
     let gender_threshold: u8 = 126;
+    let trainer_id: u16 = 54321; // Example Trainer ID
+    let trainer_secret_id: u16 = 12345; // Example Secret ID
+
     let mut rng_factory = RngFactory::new();
     let personality_value = rng_factory.generate_personality_value();
     println!("Random u32: {}", personality_value);
@@ -14,7 +18,12 @@ fn main() {
     let last_byte = (personality_value & 0xFF) as u8;
     println!("Last byte: {}", last_byte);
 
-    let some_pokemon = Pokemon::new(personality_value, gender_threshold);
+    let some_pokemon = Pokemon::new(
+        personality_value,
+        gender_threshold,
+        trainer_id,
+        trainer_secret_id,
+    );
 
     println!("{:#?}", some_pokemon);
 }
--- src/pokemon.rs
+++ src/pokemon.rs
@@ -14,19 +14,31 @@ pub struct Pokemon {
     personality_value: u32,
     gender: Gender,
     nature: natures::Nature,
+    shiny_value: u16,
+    shiny: bool,
 }
 
 impl Pokemon {
-    pub fn new(personality_value: u32, gender_threshold: u8) -> Self {
-        let gender = Pokemon::assign_gender(&personality_value, gender_threshold);
+    pub fn new(
+        personality_value: u32,
+        gender_threshold: u8,
+        trainer_id: u16,
+        trainer_secret_id: u16,
+    ) -> Self {
+        let gender = Pokemon::calculate_gender(&personality_value, gender_threshold);
         let nature = natures::determine_nature(&personality_value);
+        let shiny_value =
+            Pokemon::calculate_shiny_value(&personality_value, &trainer_id, &trainer_secret_id);
+        let shiny = shiny_value < 8;
         Pokemon {
             personality_value,
             gender,
             nature,
+            shiny_value,
+            shiny,
         }
     }
-    fn assign_gender(pvalue: &u32, gender_threshold: u8) -> Gender {
+    fn calculate_gender(pvalue: &u32, gender_threshold: u8) -> Gender {
         if gender_threshold == 255 {
             return Gender::Unknown;
         }
@@ -37,5 +49,10 @@ impl Pokemon {
         } else {
             Gender::Female
         }
+    }
+    fn calculate_shiny_value(pvalue: &u32, trainer_id: &u16, trainer_secret_id: &u16) -> u16 {
+        let pid_high = (pvalue >> 16) as u16;
+        let pid_low = (pvalue & 0xFFFF) as u16;
+        pid_high ^ pid_low ^ trainer_id ^ trainer_secret_id
     }
 }
