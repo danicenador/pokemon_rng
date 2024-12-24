03c401e989c584f3bb9f9032bddc50f6478b9bbb
Your Name <you@example.com>
2024-12-24 13:16:45 +0100

    added iv generation

--- src/pokemon.rs
+++ src/pokemon.rs
@@ -15,6 +15,7 @@ pub struct Pokemon {
     nature: natures::Nature,
     shiny_value: u16,
     shiny: bool,
+    ivs: [u8; 6],
 }
 
 impl Pokemon {
@@ -29,12 +30,14 @@ impl Pokemon {
         let shiny_value =
             Pokemon::calculate_shiny_value(&personality_value, &trainer_id, &trainer_secret_id);
         let shiny = shiny_value < 8;
+        let ivs = Pokemon::calculate_ivs(&personality_value);
         Pokemon {
             personality_value,
             gender,
             nature,
             shiny_value,
             shiny,
+            ivs,
         }
     }
     fn calculate_gender(pvalue: &u32, gender_threshold: u8) -> Gender {
@@ -53,5 +56,12 @@ impl Pokemon {
         let pid_high = (pvalue >> 16) as u16;
         let pid_low = (pvalue & 0xFFFF) as u16;
         pid_high ^ pid_low ^ trainer_id ^ trainer_secret_id
+    }
+    fn calculate_ivs(pvalue: &u32) -> [u8; 6] {
+        let mut ivs = [0; 6];
+        for i in 0..6 {
+            ivs[i] = ((pvalue >> (i * 5)) & 0x1F) as u8;
+        }
+        ivs
     }
 }
