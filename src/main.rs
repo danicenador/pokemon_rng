fe862a6bfb567053d76d8543fc04765fa2341982
Your Name <you@example.com>
2024-12-25 11:31:02 +0100

    execution time added

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 mod natures;
 mod pokemon;
 mod rng_factory;
-use std::result;
-use std::vec;
+use std::time::Instant;
 
 use pokemon::Pokemon;
 use rng_factory::RngFactory;
@@ -13,9 +12,12 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     // hardcoded input
+    let number_of_pokemon: u32 = 1_000;
     let gender_threshold: u8 = 126;
     let trainer_id: u16 = 54321; // Example Trainer ID
     let trainer_secret_id: u16 = 12345; // Example Secret ID
+
+    let start = Instant::now();
 
     let mut rng_factory = RngFactory::new();
 
@@ -28,7 +30,7 @@ fn main() -> Result<()> {
     );
     let mut result_df = first_pokemon.to_dataframe().unwrap();
 
-    for _ in 0..10_000 {
+    for _ in 0..number_of_pokemon - 1 {
         let personality_value = rng_factory.generate_personality_value();
         let some_pokemon = Pokemon::new(
             personality_value,
@@ -51,8 +53,9 @@ fn main() -> Result<()> {
     // Write the DataFrame to a CSV file
     let file = File::create("output/output.csv")?;
     CsvWriter::new(&file).finish(&mut result_df).unwrap();
-
     println!("DataFrame written to output.csv");
 
+    let duration = start.elapsed();
+    println!("Time elapsed: {:?}", duration);
     Ok(())
 }
